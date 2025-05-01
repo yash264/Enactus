972cb92c8ac9e07b392be594bad5b580a2d87bed
yash264 <yp20222068@gmail.com>
2025-05-01 16:53:49 +0530

    resolve conflicit

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,7 @@
 import React from 'react';
-import { Route, Routes, useLocation } from 'react-router-dom';
+import { Route, Routes } from 'react-router-dom';
+import { useEffect } from 'react';
+import axios from 'axios';
 
 import Home from './pages/Home';
 import AboutUs from './pages/About';
